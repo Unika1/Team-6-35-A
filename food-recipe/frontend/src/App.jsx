a043b9086a309667bd02fd83ac96b16ff6ab007a
Deepa131 <deepapaudel65@gmail.com>
2025-02-19 23:47:21 +0545

    changed App.jsx

--- food-recipe/frontend/src/App.jsx
+++ food-recipe/frontend/src/App.jsx
@@ -1,25 +1,33 @@
-import { Routes, Route } from 'react-router-dom';
-import Login from './components/public/Login';
-import Signup from './components/public/Signup';
-import Content from './components/private/Content';
-import AboutUs from './components/private/AboutUs';
+import React from 'react';
+import { Routes, Route, useLocation } from 'react-router-dom';
+import Recipe from './components/private/Recipe';
+import Meal from './components/private/Meal';
 import Homepage from './components/private/Homepage';
 import Navbar from './components/private/Navbar';
 import Footer from './components/private/Footer';
-import FrontSection from './components/private/FrontSection';
+import Login from './components/public/Login';
+import Signup from './components/public/Signup';
+import AboutUs from './components/private/AboutUs';
 
 function App() {
+  const location = useLocation();
+  const hideNavAndFooter = location.pathname === "/" || location.pathname === "/signup";
+
   return (
-    <>
+    <div>
+      {!hideNavAndFooter && <Navbar />}  {/* Show Navbar only if not on Login or Signup */}
+      
       <Routes>
-        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
-        <Route path="/content" element={<Content />} />
-        <Route path="/aboutus" element={<AboutUs />} />
-        <Route path="/homepage" element={<Homepage />} />
-        <Route path="/frontsection" element={<FrontSection />} />
+        <Route path="/home" element={<Homepage />} />
+        <Route path="/meal" element={<Meal />} />
+        <Route path="/:recipeId" element={<Recipe />} />
+        <Route path="/about" element={<AboutUs />} />
       </Routes>
-    </>
+      
+      {!hideNavAndFooter && <Footer />}  {/* Show Footer only if not on Login or Signup */}
+    </div>
   );
 }
 
