cac9a0539a65186ff6678c5008851fced882a032
iamsabinakhanal <khanalsabina442@gmail.com>
2025-03-04 22:08:01 +0545

    aboutus updated

--- food-recipe/frontend/src/components/pages/private/AboutUs.jsx
+++ food-recipe/frontend/src/components/pages/private/AboutUs.jsx
@@ -1,21 +1,21 @@
 import React from 'react';
-// import '../../styles/AboutUs.css';
-// import chiefLogo from '../../assets/images/chief.png';
-// import shreetika from '../../assets/images/shreetika.jpg';
-// import dipika from '../../assets/images/dipika.png';
-// import sabina from '../../assets/images/sabina.png';
-// import deepa from '../../assets/images/deepa.jpeg';
-// import unika from '../../assets/images/unika.jpg';
-// import dalbhaat from '../../assets/images/dalbhaat.jpg';
-// import kheer from '../../assets/images/kheer.jpg';
-// import nepalifood from '../../assets/images/nepalifood.jpeg';
-// import newari from '../../assets/images/newari.jpg';
-// import gundruk from '../../assets/images/gundruk.jpg';
+import '../../../styles/AboutUs.css';
+import chiefLogo from '../../../assets/images/chief.png';
+import shreetika from '../../../assets/images/shreetika.jpg';
+import dipika from '../../../assets/images/dipika.png';
+import sabina from '../../../assets/images/sabina.png';
+import deepa from '../../../assets/images/deepa.jpeg';
+import unika from '../../../assets/images/unika.jpg';
+import dalbhaat from '../../../assets/images/dalbhaat.jpg';
+import kheer from '../../../assets/images/kheer.jpg';
+import nepalifood from '../../../assets/images/nepalifood.jpeg';
+import newari from '../../../assets/images/newari.jpg';
+import gundruk from '../../../assets/images/gundruk.jpg';
 
 const AboutUs = () => {
   return (
     <div className="about-us-container">
-      {/* <header className="chief-logo">
+      <header className="chief-logo">
         <img src={chiefLogo} alt="Chief Logo" />
       </header>
 
@@ -27,24 +27,24 @@ const AboutUs = () => {
         <div className="written">
           <p>We are thrilled to introduce our Nepali Cuisine Recipe Book...</p>
         </div>
-        <div className="shreetika">
-          <h3>SHREETIKA SHRESTHA</h3>
-          <img src={shreetika} alt="Shreetika" />
-        </div>
-        <div className="dipika">
-          <h3>DIPIKA MAHARJAN</h3>
-          <img src={dipika} alt="Dipika" />
-        </div>
-        <div className="sabina">
-          <h3>SABINA KHANAL</h3>
-          <img src={sabina} alt="Sabina" />
-        </div>
         <div className="deepaunika">
-          <div className="deepa">
+          <div>
+            <h3>SHREETIKA SHRESTHA</h3>
+            <img src={shreetika} alt="Shreetika" />
+          </div>
+          <div>
+            <h3>DIPIKA MAHARJAN</h3>
+            <img src={dipika} alt="Dipika" />
+          </div>
+          <div>
+            <h3>SABINA KHANAL</h3>
+            <img src={sabina} alt="Sabina" />
+          </div>
+          <div>
             <h3>DEEPA POUDEL</h3>
             <img src={deepa} alt="Deepa" />
           </div>
-          <div className="unika">
+          <div>
             <h3>UNIKA PARAJULI</h3>
             <img src={unika} alt="Unika" />
           </div>
@@ -54,13 +54,16 @@ const AboutUs = () => {
       <section className="food-container">
         <h2>Favorite Food</h2>
         <p>Explore our favorite recipes...</p>
-        <img src={dalbhaat} alt="Dal Bhaat" />
-        <img src={kheer} alt="Kheer" />
-        <img src={nepalifood} alt="Nepali Food" />
-        <img src={newari} alt="Newari Food" />
-        <img src={gundruk} alt="Gundruk" />
-      </section> */}
+        <div className="food-images">
+          <img src={dalbhaat} alt="Dal Bhaat" />
+          <img src={kheer} alt="Kheer" />
+          <img src={nepalifood} alt="Nepali Food" />
+          <img src={newari} alt="Newari Food" />
+          <img src={gundruk} alt="Gundruk" />
+        </div>
+      </section>
     </div>
   );
 };
-export default AboutUs
+
+export default AboutUs;
